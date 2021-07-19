f51e350b571cde8e34691d26822648ef61fa691e
sbigelo <sean.bigelow1@gmail.com>
2021-07-19 13:35:59 -0400

    commit tree 1dc53b31dd03e0a0c067557f901a720dfeb1e07d

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -2,8 +2,37 @@ import React, { Component } from 'react';
 import 'isomorphic-fetch';
 import MovieReviews from './MovieReviews'
 
-const NYT_API_KEY = 'your-key-here';
+const NYT_API_KEY = 'Z5c5bQzFVaiw4TxKT5EChyCMc85RpYZT';
 const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
             + `api-key=${NYT_API_KEY}`;
 
 // Code LatestMovieReviewsContainer Here
+
+class LatestMovieReviewsContainer extends Component {
+
+    constructor() {
+        super()
+        this.state = {
+            reviews: []
+        }
+    }
+
+    componentDidMount() {
+        fetch(URL)
+        .then(resp => resp.json())
+        .then(data  => this.setState({reviews: data.results}))
+    }
+
+    render() {
+        return(
+            <div className="latest-movie-reviews">
+                <h2>Latest Reviews</h2>
+                <MovieReviews reviews={this.state.reviews}/>
+            </div>
+        )
+    }
+
+}
+
+
+export default LatestMovieReviewsContainer
