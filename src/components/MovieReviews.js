782f33c16f0a67d015f991a94d7541fb0016b011
sbigelo <sean.bigelow1@gmail.com>
2021-07-19 14:20:28 -0400

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,31 @@
-// Code MovieReviews Here
+import React from 'react';
+
+const Review = ({
+    headline,
+    byline,
+    link,
+    summary_short
+}) => {
+      return(
+            <div
+            key={headline}
+            className='review'>
+                <header>
+                <h1>{headline}</h1>
+                <p className='author'>{byline}</p>
+                <a className='review-link'
+                href={link.url}>{headline}</a>
+              </header>
+                <p>{summary_short}</p>
+            </div>
+        )
+    
+
+}
+
+const MovieReviews = ({ reviews }) => <div className='review-list'>{reviews.map(Review)}</div>
+MovieReviews.defaultProps = {
+    reviews: []
+}
+
+export default MovieReviews
--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -2,8 +2,49 @@ import React, { Component } from 'react';
 import 'isomorphic-fetch';
 import MovieReviews from './MovieReviews'
 
-const NYT_API_KEY = 'your-key-here';
+const NYT_API_KEY = 'Z5c5bQzFVaiw4TxKT5EChyCMc85RpYZT';
 const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?'
             + `api-key=${NYT_API_KEY}`;
 
 // Code SearchableMovieReviewsContainer Here
+
+class SearchableMovieReviewsContainer extends Component {
+
+    state = {
+        searchTerm: '',
+        reviews: []
+    }
+
+
+
+    handleSubmit = event => {
+        event.preventDefault();
+debugger
+        fetch(URL.concat(this.state.searchTerm)) 
+        .then(resp => resp.json())
+        .then(data => data.setState({reviews: data.results}))
+
+    } 
+
+    handleSearchChange = event => {
+        this.setState({searchTerm: event.target.value})
+    }
+
+
+    render() {
+        return(
+            <div>
+                <form onSubmit={this.handleSubmit}>
+                <input type='text' value={this.state.searchTerm} 
+                onChange={this.handleSearchChange}></input>
+                </form>
+                <MovieReviews reviews={this.state.reviews}/>
+                
+            </div>
+        )
+    }
+
+
+}
+
+export default SearchableMovieReviewsContainer 
